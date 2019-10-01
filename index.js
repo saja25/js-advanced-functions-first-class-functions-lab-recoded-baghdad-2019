c7e84eb4af2c2053658b05888a67857224b6f957
saja amer <sajaamer86@gmail.com>
2019-10-01 11:57:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers=function (arr){
     return arr.slice(0,2) ;
-}
+};
 const returnLastTwoDrivers=function(arr){
   return arr.slice(-2) ;
-}
+};
 let selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers] ;
